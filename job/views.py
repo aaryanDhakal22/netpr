cb5a4b48470ae670524258be38865564615f8ee5
aaryanDhakal22 <aaryan.dhakal22@gmail.com>
2025-01-20 03:44:51 -0500

    Added some file upload code

--- job/views.py
+++ job/views.py
@@ -4,6 +4,11 @@ from django.shortcuts import render
 def homepage(req):
     return render(req,"job/homepage.html")
 
+def handle_upload(file):
+    filename = file.name
+    print(filename)
 
-def print_job(req):
-    return render(req,"job/success.html")
+def upload_file(req):
+    if req.method == "POST":
+        handle_upload(req.FILES["file"])
+        return render(req,"job/success.html")
